6aa703a8c787e3b850b1e8af8c611fb52c686760
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-11 15:43:25 -0800

    Have a basic schema passing
    I need to add more to classes. That is the only one that seems broken, meaning no matter what I do it will pass. All of the other properties are working.

---
+++ schemas.py
@@ -0,0 +1,71 @@
+def build_student_class_schema(target_json: dict) -> dict:
+    # classes = build_classes_property(target_json)
+    schema = {
+        "definitions": {
+            "student": {
+                "type": "object",
+                "properties": {
+                        "first": {"type": "string"},
+                        "last": {"type": "string"},
+                        "email": {"type": "string"},
+                        "studentClasses": {
+                            "type": "array",
+                            "items": {"$ref": "#/definitions/studentClasses"}
+                        }
+                },
+                "additionalProperties": False,
+            },
+            "studentClasses": {
+                "type": "object",
+                "properties": {
+                        "id": {"type": "number", "multipleOf": 1},
+                        "grade": {
+                            "type": "number",
+                            "multipleOf": 0.5,
+                            "minimum": 1,
+                            "maximum": 4,
+                        }
+                },
+                "additionalProperties": False,
+            },
+        },
+        "type": "object",
+        "properties": {
+            "students": {
+                "type": "array",
+                "items": {"$ref": "#/definitions/student"}
+            },
+            "classes": {
+                "type": "object"
+            },
+        },
+        "additionalProperties": False,
+    }
+
+    return schema
+
+
+def build_classes_property(target_json):
+    default = {
+        "1": {"type": "string"},
+        "2": {"type": "string"},
+        "3": {"type": "string"},
+        "4": {"type": "string"},
+        "5": {"type": "string"},
+        "6": {"type": "string"},
+        "7": {"type": "string"},
+        # "8": {"type": "string"}
+    }
+    custom = {}
+
+    try:
+        for item in target_json["classes"]:
+            print("hey")
+            print(item)
+            custom[item.key] = {"type": "string"}
+    except:
+        print("what")
+        custom = default
+
+    # print(custom)
+    return custom
--- test_student_classes.py
+++ test_student_classes.py
@@ -1,13 +1,24 @@
 import unittest
 import json
+from jsonschema import validate
+from schemas import build_student_class_schema
+
+
+def get_json_object(file_path: str) -> dict:
+    with open(file_path) as file:
+        try:
+            test_json = json.load(file)
+        except json.decoder.JSONDecodeError:
+            test_json = {}
+    return test_json
 
 
 class StudentClassResponse(unittest.TestCase):
-    def test_valid_json(self):
-        valid = True
-        with open("fixtures/students_classes.json") as file:
-            try:
-                json.load(file)
-            except json.decoder.JSONDecodeError:
-                valid = False
-        self.assertTrue(valid)
+    @classmethod
+    def setUpClass(cls):
+        cls.student_json = get_json_object(
+            "fixtures/students_classes.json")
+
+    def test_json_schema(self):
+        schema = build_student_class_schema(self.student_json)
+        validate(instance=self.student_json, schema=schema)
