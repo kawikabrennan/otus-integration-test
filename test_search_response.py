6ce81d508d76f25997a5999de9914d2a71f80d16
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-11 21:07:02 -0800

    updated tests to be more clear

--- test_search_response.py
+++ test_search_response.py
@@ -39,69 +39,66 @@ class RainyDaySearchResponse(unittest.TestCase):
     def setUp(self):
         self.schema = copy.deepcopy(ASSESSMENT_SEARCH_RESPONSE_SCHEMA)
 
-    def test_json_assessment_title_length_restriction(self):
-        """Altering schema to show edge case testing.
-        Update custom schema to expect untouched response data to have smaller title length.
-        """
+    def result_validator(self, altered_schema: dict):
         error = False
 
-        # schema = copy.deepcopy(ASSESSMENT_SEARCH_RESPONSE_SCHEMA)
-        self.schema["definitions"]["assessment"]["properties"]["assessment_title"] = {
-            "type": "string", "maxLength": 10
-        }
         try:
-            validate(instance=self.response_json, schema=self.schema)
+            validate(instance=self.response_json, schema=altered_schema)
         except ValidationError:
             error = True
 
         self.assertTrue(error)
+
+    def test_json_assessment_title_length_restriction(self):
+        """Altering schema to show edge case testing.
+        Update custom schema to expect untouched response data to have smaller title length.
+        """
+        self.schema["definitions"]["assessment"]["properties"]["assessment_title"] = {
+            "type": "string", "maxLength": 10
+        }
+        self.result_validator(self.schema)
+
+    def test_json_assessment_title_regex(self):
+        """Altering schema to show edge case testing.
+        Update custom schema to expect untouched response data to match a pattern.
+        """
+        self.schema["definitions"]["assessment"]["properties"]["assessment_title"] = {
+            "type": "string", "pattern": "^[a-zA-Z]+$"
+        }
+        self.result_validator(self.schema)
 
     def test_json_assessment_type_change(self):
         """Altering schema to show edge case testing.
         Update custom schema to expect untouched response data to have different type.
         """
-        error = False
-
-        # schema = ASSESSMENT_SEARCH_RESPONSE_SCHEMA
         self.schema["definitions"]["assessment"]["properties"]["assessment_type"] = {
             "type": "string"
         }
-        try:
-            validate(instance=self.response_json, schema=self.schema)
-        except ValidationError:
-            error = True
+        self.result_validator(self.schema)
 
-        self.assertTrue(error)
+    def test_json_assessment_type_range(self):
+        """Altering schema to show edge case testing.
+        Update custom schema to expect untouched response data to be out of range.
+        """
+        self.schema["definitions"]["assessment"]["properties"]["assessment_type"] = {
+            "type": "number", "maximum": 1
+        }
+        self.result_validator(self.schema)
 
     def test_json_district_id_type(self):
         """Altering schema to show edge case testing.
         Update custom schema to expect untouched response data to be a string.
         """
-        error = False
-
-        # schema = ASSESSMENT_SEARCH_RESPONSE_SCHEMA
         self.schema["definitions"]["assessment"]["properties"]["district_id"] = {
             "type": "string"
         }
-        try:
-            validate(instance=self.response_json, schema=self.schema)
-        except ValidationError:
-            error = True
-
-        self.assertTrue(error)
+        self.result_validator(self.schema)
 
     def test_json_district_id_maximum(self):
         """Altering schema to show edge case testing.
         Update custom schema to expect untouched response data to be a maxium of 1.
         """
-        error = False
-
         self.schema["definitions"]["assessment"]["properties"]["district_id"] = {
-            "type": "number", "maximum": 1
+            "type": "number", "maximum": 900
         }
-        try:
-            validate(instance=self.response_json, schema=self.schema)
-        except ValidationError:
-            error = True
-
-        self.assertTrue(error)
+        self.result_validator(self.schema)
