a6131382e6dc4446d599810ff3b6490e7fd83b9b
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-11 16:45:29 -0800

    updated the test name

--- test_student_classes.py
+++ test_student_classes.py
@@ -19,6 +19,7 @@ class StudentClassResponse(unittest.TestCase):
         cls.student_json = get_json_object(
             "fixtures/students_classes.json")
 
-    def test_json_schema(self):
+    def test_json_data_validation(self):
+        """Verify JSON data against a schema"""
         schema = build_student_class_schema(self.student_json)
         validate(instance=self.student_json, schema=schema)
