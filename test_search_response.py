c495ece680e83ad682a65239e5ac091bec5cb3a3
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-11-11 20:20:07 -0800

    created a "rainy day" scenario class
    I am manipulating my existing schema to intentionally create an error. There isn't a backend to test against, so I need to make up requirements to show that I know edge case testing.

--- test_search_response.py
+++ test_search_response.py
@@ -1,6 +1,7 @@
 import unittest
 from helpers import get_json_object
 from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 from schemas import ASSESSMENT_SEARCH_RESPONSE_SCHEMA
 from schema_definitions import ASSESSMENT
 
@@ -26,3 +27,25 @@ class AssessmentSearchResponse(unittest.TestCase):
                 validate(instance=assessment, schema=ASSESSMENT)
 
         self.assertEqual(len(validate_test_id), 1)
+
+
+class RainyDaySearchResponse(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.response_json = get_json_object(
+            "fixtures/search_response.json")
+
+    def test_json_assessment_title_length_restriction(self):
+        """Verify data against the rainy day schema"""
+        error = False
+
+        schema = ASSESSMENT_SEARCH_RESPONSE_SCHEMA
+        schema["definitions"]["assessment"]["properties"]["assessment_title"] = {
+            "type": "string", "maxLength": 10
+        }
+        try:
+            validate(instance=self.response_json, schema=schema)
+        except ValidationError:
+            error = True
+
+        self.assertTrue(error)
